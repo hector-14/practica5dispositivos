d8404e96b2423fea141aa4976c76a5a2e3d41d4a
hector-14 <63135688+hector-14@users.noreply.github.com>
2022-09-28 15:18:45 -0500

    Add files via upload

---
+++ InicialActivity.java
@@ -0,0 +1,16 @@
+package mx.edu.tesoem.itics.p57s2120222hmrm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InicialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicial);
+
+
+    }
+}
---
+++ SumaActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.itics.p57s2120222hmrm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SumaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suma);
+    }
+}
