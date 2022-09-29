852ab6b8a33d7e5c15872c914fa2ce00e10a4198
hector-14 <63135688+hector-14@users.noreply.github.com>
2022-09-28 23:22:08 -0500

    Add files via upload

--- InicialActivity.java
+++ InicialActivity.java
@@ -1,16 +1,27 @@
-package mx.edu.tesoem.itics.p57s2120222hmrm;
+package mx.edu.tesoem.isc.p5y67s2120222hmrm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class InicialActivity extends AppCompatActivity {
 
+    Handler manejador = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicial);
-
-
+        manejador.postDelayed(autocambiar, 5000);
     }
+
+    Runnable autocambiar = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(InicialActivity.this,PrincipalActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 }
---
+++ PrincipalActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.isc.p5y67s2120222hmrm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
